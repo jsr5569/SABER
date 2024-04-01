e9216c6e2c313fcbad6e7514aa4802dc63948bd6
Kendall Roy <kendallroy@blonded.local>
2024-04-01 15:02:27 -0400

    new dynamodb

--- src/gamepage.js
+++ src/gamepage.js
@@ -48,7 +48,7 @@ function GamePage({ game }) {
         <img src={game.src} alt={game.title} className="game-image" />
         <div className="description">
           <p>Description:
-            {game.description}.</p>
+            {game.description}</p>
           <p>Release Date: {game.releaseDate}</p>
           <p>Genre: {game.genre}</p>
           <p>Platform: {game.platform}</p>
